2b5d8c2a4d8823ecb7115152fd258af95b0a377e
edennnni <edenkurtolli7@gmail.com>
2025-06-12 16:05:36 +0200

    whopp

--- footer.php
+++ footer.php
@@ -1,2 +1,15 @@
+<footer class="site-footer">
+   <div class="container">
+        <div class="copyright">
+            <p> <?php get_theme_mod('set_copyright','copyright x-all rights reserved');?></p>
+        </div>
+        <nav class="footer-menu">
+            <?php wp_nav_menu(array('theme_location'=>__('wp_devs','wp-devs_footer_menu'),'depth'=> '1',''=> '1'));?>
+        </nav>
+   </div>
+<?php wp_footer();?>
+</footer>
+
+
 </html>
 <body>
